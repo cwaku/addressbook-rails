b4e562e3ea4b263834b94b624d0cdd2d6055eab8
Ricky Mormor <ricky@appsnmobilesolutions.com>
2023-02-27 14:28:20 +0000

    generate user role controller, model and migrations

---
+++ app/views/user_roles/_user_role.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! user_role, :id, :role_code, :user_id, :active_status, :del_status, :created_at, :updated_at, :created_at, :updated_at
+json.url user_role_url(user_role, format: :json)
---
+++ db/migrate/20230227142801_create_user_roles.rb
@@ -0,0 +1,14 @@
+class CreateUserRoles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :user_roles do |t|
+      t.string :role_code
+      t.integer :user_id
+      t.boolean :active_status
+      t.boolean :del_status
+      t.timestamp :created_at
+      t.timestamp :updated_at
+
+      t.timestamps
+    end
+  end
+end
