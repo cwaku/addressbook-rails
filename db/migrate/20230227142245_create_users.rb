f47963f4526d137f14be02e5ffb8bd32cb6dc4c2
Ricky Mormor <ricky@appsnmobilesolutions.com>
2023-02-27 14:23:46 +0000

    generate user

---
+++ db/migrate/20230227142245_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :email
+      t.boolean :active_status
+      t.boolean :del_status
+      t.timestamp :created_at
+      t.timestamp :updated_at
+
+      t.timestamps
+    end
+  end
+end
