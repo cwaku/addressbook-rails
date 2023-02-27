f72fd68ae8184a5f4859eb58a3fc87d90baed795
Ricky Mormor <ricky@appsnmobilesolutions.com>
2023-02-27 17:18:18 +0000

    Fix linter errors

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -4,31 +4,47 @@ class RolesController < ApplicationController
   before_action :set_role, only: %i[show edit update destroy]
   before_action :load_permissions
   load_and_authorize_resource
+  before_action :super_admin?
+
   # GET /roles or /roles.json
   def index
-    @roles = Role.all
+    @roles = Role.order('id asc').all
   end
 
   # GET /roles/1 or /roles/1.json
-  def show; end
+  def show
+    # @role = Role.find(params[:id])
+  end
 
   # GET /roles/new
   def new
+    @entity_info_search = EntityInfo.where(active_status: true).order(entity_name: :asc)
+
     @role = Role.new
+    @permissions = Permission.all.where("subject_class !='Role'").compact
+    @role_permissions = @role.permissions.collect(&:id)
   end
 
   # GET /roles/1/edit
-  def edit; end
+  def edit
+    @permissions = Permission.where("id != '1'").where("subject_class !='Role'").compact
+    @role_permissions = @role.permissions.collect(&:id)
+  end
 
   # POST /roles or /roles.json
   def create
     @role = Role.new(role_params)
+    @role = Role.new(role_params)
+    @role.permissions = []
+    @role.set_permissions(params[:permissions]) if params[:permissions]
 
     respond_to do |format|
       if @role.save
         format.html { redirect_to role_url(@role), notice: 'Role was successfully created.' }
         format.json { render :show, status: :created, location: @role }
       else
+        @permissions = Permission.where("id != '1'").where("subject_class !='Role'").compact
+        @role_permissions = @role.permissions.collect(&:id)
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @role.errors, status: :unprocessable_entity }
       end
@@ -37,11 +53,16 @@ class RolesController < ApplicationController
 
   # PATCH/PUT /roles/1 or /roles/1.json
   def update
+    @role.permissions = []
+    @role.set_permissions(params[:permissions]) if params[:permissions]
+
     respond_to do |format|
       if @role.update(role_params)
         format.html { redirect_to role_url(@role), notice: 'Role was successfully updated.' }
         format.json { render :show, status: :ok, location: @role }
       else
+        @permissions = Permission.where("id != '1'").where("subject_class !='Role'").compact
+        @role_permissions = @role.permissions.collect(&:id)
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @role.errors, status: :unprocessable_entity }
       end
@@ -68,5 +89,11 @@ class RolesController < ApplicationController
   # Only allow a list of trusted parameters through.
   def role_params
     params.require(:role).permit(:name, :unique_code, :active_status, :del_status)
+  end
+
+  def super_admin?
+    return true if current_user.role.name == 'Super Admin'
+
+    redirect_to root_path, notice: 'You are not authorized to access this page.'
   end
 end
