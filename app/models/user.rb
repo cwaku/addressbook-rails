5c629eaa4afeefe4a44c1fe69bc41ec47cb4c3bb
Ricky Mormor <ricky@appsnmobilesolutions.com>
2023-02-27 16:29:22 +0000

    Configure devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :user_roles, -> { where active_status: true }, class_name: "UserRole", foreign_key: :user_id
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   resources :permissions_roles
   resources :permissions
   resources :user_roles
