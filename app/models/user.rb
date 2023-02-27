33d3ef041e5645bb70588d229dcdcfd2f3919780
Ricky Mormor <ricky@appsnmobilesolutions.com>
2023-02-27 14:55:31 +0000

    Working on associations between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
+# Creates a new UserRole instance.
 class User < ApplicationRecord
+  has_many :user_roles, -> { where active_status: true }, class_name: "UserRole", foreign_key: :user_id
 end
--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
+# Creates a new UserRole object.
 class UserRole < ApplicationRecord
+  belongs_to :role, -> { where active_status: true }, class_name: "Role", foreign_key: :role_code, primary_key: :unique_code
+  belongs_to :user, -> { where active_status: true }, class_name: "User", foreign_key: :user_id
 end
